d1bd51398cf6987b970bc40480ca6f88cde14a2a
Magnus Johansson <pgmj@pm.me>
2026-02-03 08:35:41 +0100

    Replace item fit analysis with rasch_itemfit function

--- R/itemfit.b.R
+++ R/itemfit.b.R
@@ -27,58 +27,8 @@ itemfitClass <- R6::R6Class(
             # Run the analysis
             tryCatch({
                 # Get fit statistics using simulations
-                gf <- RIgetfit(df, iterations = iterations, cpu = cores, seed = seed)
-                
-                # Get item fit table (recreate logic from RIitemfit without kable output)
-                if(max(as.matrix(df), na.rm = TRUE) == 1) {
-                    erm_out <- eRm::RM(df)
-                    item_avg_locations <- coef(erm_out, "beta") * -1
-                    person_avg_locations <- eRm::person.parameter(erm_out)[["theta.table"]][["Person Parameter"]] %>%
-                        mean(na.rm = TRUE)
-                    relative_item_avg_locations <- item_avg_locations - person_avg_locations
-                } else if(max(as.matrix(df), na.rm = TRUE) > 1) {
-                    # For polytomous data, use PCM and RIestThetasOLD (catR-based estimation)
-                    erm_out <- eRm::PCM(df)
-                    item_avg_locations <- RIitemparams(df, output = "dataframe") %>%
-                        pull(Location)
-                    person_avg_locations <- RIestThetasOLD(df) %>%
-                        mean(na.rm = TRUE)
-                    relative_item_avg_locations <- item_avg_locations - person_avg_locations
-                }
-                
-                # Get conditional MSQ
-                cfit <- iarm::out_infit(erm_out)
-                
-                # Get item parameters for thresholds
-                item_params <- RIitemparams(df, output = "dataframe")
-                
-                # Get the fit table
-                table <- self$results$fitTable
-                
-                # Populate the table
-                for (i in seq_along(vars)) {
-                    item_name <- vars[i]
-                    
-                    # Get infit MSQ value
-                    infitmsq <- round(cfit$Infit[i], 3)
-                    
-                    # Get relative location
-                    location <- round(relative_item_avg_locations[i], 2)
-                    
-                    # Get thresholds as text
-                    threshold_cols <- grep("^Threshold", names(item_params), value = TRUE)
-                    thresholds <- item_params[i, threshold_cols]
-                    threshold_text <- paste(round(na.omit(as.numeric(thresholds)), 2), collapse = ", ")
-                    
-                    # Add row to table
-                    table$setRow(rowNo = i, values = list(
-                        item = item_name,
-                        infitmsq = infitmsq,
-                        location = location,
-                        thresholds = threshold_text
-                    ))
-                }
-                
+                rasch_itemfit(df, iterations = iterations, cpu = cores, seed = seed)
+
             }, error = function(e) {
                 # Handle errors gracefully
                 stop(paste("Error in item fit analysis:", e$message))
